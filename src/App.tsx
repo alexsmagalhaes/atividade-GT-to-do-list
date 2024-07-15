f802697572fffe5f6263c115020757a76fb57d4e
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-07-14 22:53:43 -0300

    init( add initial config)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import TodoCard from "./components/ToDoCard"
+import TodoList from "./components/ToDoList"
+
 function App() {
   return (
-    <>
-      <div className="text-slate-700">Olá</div>
-    </>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <TodoCard />
+    </div>
   )
 }
 
